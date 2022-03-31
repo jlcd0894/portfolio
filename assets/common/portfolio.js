077330338320875b7378d9b4aebdadc59afaab42
jlcd0894 <jlcdominic94@gmail.com>
2022-03-31 13:07:13 -0700

    typewriter test

--- assets/common/portfolio.js
+++ assets/common/portfolio.js
@@ -45,6 +45,22 @@ if (isMobile===true) {
         //
         TweenLite.set(wave, {attr:{fill: settings.color}});
 
+        var app = document.getElementById('app');
+
+        var typewriter = new Typewriter(app, {
+            loop: true
+        });
+
+        typewriter.typeString('Hello World!')
+            .pauseFor(2500)
+            .deleteAll()
+            .typeString('Strings can be removed')
+            .pauseFor(2500)
+            .deleteChars(7)
+            .typeString('<strong>altered!</strong>')
+            .pauseFor(2500)
+            .start();
+
 
         function drawPoints(factor) {
             var points = [];
