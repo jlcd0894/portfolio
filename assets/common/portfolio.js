9509acd3b3154b371fb335a9b04421fe4fb76098
jlcd0894 <jlcdominic94@gmail.com>
2022-03-31 13:14:05 -0700

    fix 2

--- assets/common/portfolio.js
+++ assets/common/portfolio.js
@@ -187,21 +187,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-        var app = document.getElementById('app');
+var app = document.getElementById('app');
 
-        var typewriter = new Typewriter(app, {
-            loop: true
-        });
+var typewriter = new Typewriter(app, {
+    loop: true
+});
 
-        typewriter.typeString('Hello World!')
-            .pauseFor(2500)
-            .deleteAll()
-            .typeString('Strings can be removed')
-            .pauseFor(2500)
-            .deleteChars(7)
-            .typeString('<strong>altered!</strong>')
-            .pauseFor(2500)
-            .start();
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
 
 
 var animation = lottie.loadAnimation({
