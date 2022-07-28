c810324cbbf1d5191cb74a9fc2a6f90940477acb
jlcd0894 <jlcdominic94@gmail.com>
2022-07-28 10:38:59 -0700

    scroll js fix ?

--- assets/common/portfolio.js
+++ assets/common/portfolio.js
@@ -32,7 +32,18 @@ $(document).on('click',function(){
 })
 
 //video play when visible
- $(window).scroll(function() {
+//  $(window).scroll(function() {
+//     $('video').each(function(){
+//         if ($(this).is(":in-viewport")) {
+//             $(this)[0].play();
+//         } else {
+//             $(this)[0].pause();
+//         }
+//     })
+// });
+
+ if ($("body").data("enable-scroll"))
+    $(window).scroll(function() {
     $('video').each(function(){
         if ($(this).is(":in-viewport")) {
             $(this)[0].play();
@@ -40,7 +51,8 @@ $(document).on('click',function(){
             $(this)[0].pause();
         }
     })
-});
+      
+    });
 
 
 // // init Masonry
